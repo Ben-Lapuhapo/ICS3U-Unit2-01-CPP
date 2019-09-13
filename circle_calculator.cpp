41e634c98740f00723fd26950479303c641858be
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-09-13 02:29:54 +0000

    Added circle calculator program

---
+++ circle_calculator.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 St. Mother Teresa HS All Rights Reserved.
+//
+// Created By: Ben Lapuhapo.
+// Created on: Sep 2019.
+// This program calculates the area and perimeter of a rectangle
+
+#include <iostream>
+#include <cmath>  //  You need to add this library for pow() function.
+
+int main() {
+    std::cout << "The circle's dimensions is R = 15" << std::endl;
+    std::cout << "The circle's perimeter is " << 2*3.14*15 << std::endl;
+    std::cout << "The circle's area is " << 3.14*15*15 << std::endl;
+}
